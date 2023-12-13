41d0cad4853d29c57fb84633f610c3554faefb96
Life is squid <rredzer000@gmail.com>
2023-12-13 09:15:50 +0000

    manyTomany entity and some controller logic

---
+++ Domain.Models/Tables/UserEvent.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Domain.Models.Tables
+{
+    public class UserEvent : BaseModel
+    {
+        [JsonIgnore]
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; }
+        public Guid UserId { get; set; }
+
+        [JsonIgnore]
+        [ForeignKey(nameof(EventId))]
+        public Event Event { get; set; }
+        public Guid EventId { get; set; }
+    }
+}
